0a15715307aac900c4be5d2c35f1fb374ca9edc2
furkangu <guneysufurkan@gmail.com>
2020-04-10 23:43:24 +0300

    Singleton Update

---
+++ Creational/Singleton/Singleton.java
@@ -0,0 +1,21 @@
+package Creational.Singleton;
+
+/**
+ * Singleton
+ */
+public class Singleton {
+
+    private static Singleton instance = null;
+    
+    private Singleton(){
+
+    }
+
+    public static Singleton getSingleton(){
+        if(instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+    
+}
