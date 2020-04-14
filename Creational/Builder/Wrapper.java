a0b6f1c7e6173c0a029ca6273efefdfedfc52235
furkangu <guneysufurkan@gmail.com>
2020-04-14 23:53:13 +0300

    Update Builder

---
+++ Creational/Builder/Wrapper.java
@@ -0,0 +1,11 @@
+package Creational.Builder;
+
+public class Wrapper implements Packing {
+
+    @Override
+    public String pack() {
+        // TODO Auto-generated method stub
+        return "Wrapper";
+    }
+    
+}
