d11a01ec0dcae167c16328ee6ed46f8d4ec07442
furkangu <guneysufurkan@gmail.com>
2020-04-19 00:22:53 +0300

    AbstractFactory Update

--- Creational/AbstractFactory/FactoryProducer.java
+++ Creational/AbstractFactory/FactoryProducer.java
@@ -1,6 +1,7 @@
 package Creational.AbstractFactory;
 
 public class FactoryProducer {
+  //Will be changed
     public static AbstracfFactory getFactory(boolean rounded){
         if(rounded){
           return new RoundedShapeFactory();  
