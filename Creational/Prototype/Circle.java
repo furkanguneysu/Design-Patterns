9de423c8191f8c300b28d1ab72815f0ef226abd9
furkangu <guneysufurkan@gmail.com>
2020-04-21 22:36:06 +0300

    Update Prototype

---
+++ Creational/Prototype/Circle.java
@@ -0,0 +1,15 @@
+package Creational.Prototype;
+
+public class Circle extends Shape {
+    public Circle(){
+        type="Circle";
+    }
+
+    @Override
+    void draw() {
+        // TODO Auto-generated method stub
+        System.out.println("I am CIRCLE");
+    }
+
+    
+}
---
+++ Creational/Prototype/Square.java
@@ -0,0 +1,13 @@
+package Creational.Prototype;
+
+public class Square extends Shape {
+    public Square(){
+        type="Square";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("I am CIRCLE");
+
+    }
+}
